a50341024695f5d15cc1690c6d30a5489c8f277e
drost-dev <andrey.06.ezdakov@gmail.com>
2025-05-26 16:55:57 +0700

    created mult.py & added case 3 to main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import add, subtract
+import add, subtract, mult
 
 while 1:
     action = input('1 - сложение\n2 - вычитание\n3 - умножение\n4 - деление\n>> ')
@@ -10,4 +10,7 @@ while 1:
             print(f'результат: {r}')
         case '2':
             r = subtract(a, b)
+            print(f'результат: {r}')
+        case '3':
+            r = mult(a, b)
             print(f'результат: {r}')
